c6bc5befc68643fbb8e4af4fe44f358e63957f0c
Rowland, Derek <derek.rowland@garmin.com>
2019-10-02 13:15:26 -0500

    Removed comments and console log from public/js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,3 @@
-console.log('Client side javascript file is loaded.')
-
-// fetch('http://127.0.0.1:3000/weather?address=Boston').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }
-//     })
-        
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
